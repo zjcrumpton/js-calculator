b794e70c15a9b991f139208929f4abe5f5f474b8
Zac <zjcrumpton@gmail.com>
2020-03-23 21:59:06 -0600

    Add basic operator functions and fix title, script, and style links

---
+++ scripts.js
@@ -0,0 +1,19 @@
+//Add Function
+function add(firstValue, secondValue) {
+    return (firstValue + secondValue)
+}
+
+//Subtract Function
+function subtract(firstValue, secondValue) {
+    return (firstValue - secondValue)
+}
+
+//Multiply Function
+function multiply(firstValue, secondValue) {
+    return (firstValue * secondValue)
+}
+
+//Divide Function
+function divide(firstValue, secondValue) {
+    return (firstValue / secondValue)
+}
