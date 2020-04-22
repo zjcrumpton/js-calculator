c1736f7c8e7119d8d87f0178031f6a4462964a3a
Zac <zjcrumpton@gmail.com>
2020-04-22 15:16:19 -0600

    Add display and math function updates

--- scripts.js
+++ scripts.js
@@ -1,50 +1,98 @@
 //Global Variables
 let operator = undefined;
-let firstNumber = document.querySelector("#fNumber").value;
-let secondNumber = document.querySelector("#sNumber").value;
+let firstNumber = ""
+let secondNumber = ""
+//Defines Sub-Display so it can be updated - use subDisplay.textContent to update the display
+let subDisplay = document.querySelector("#subDisplay");
+//Clears Sub-Display On Load
+subDisplay.textContent = ""
 //Converts the input values into numbers so that they can be operated upon properly
-let firstInt = parseInt(firstNumber, 10)
-let secondInt = parseInt(secondNumber, 10)
+let firstFloat = parseFloat(firstNumber);
+let secondFloat = parseFloat(secondNumber);
+//Display - use display.value to act on what's in the display box
+let display = document.querySelector("#display");
+let displayValue = document.querySelector("#display").value;
+//Clears Text in the Display Box on page reload
+display.value = ""
 
-//Adds an event listener to the operator buttons - then assigns the proper value to the operator variable
+//Creates a state change to allow for multiple operators in use
+let opState = 0;
+let lastOp = "";
+
+//Adds an event listener to the operator buttons - then assigns the proper value to the operator variable - then adds them to the display - it's set to one once we've used at least one operator
 let operatorNodeList = document.querySelectorAll(".opBtn");
 operatorArray = Array.from(operatorNodeList)
 for (let i = 0; i < operatorArray.length; i++) {
     operatorArray[i].addEventListener("click", function() {
-        if (operatorArray[i].value == "+") {
-            operator = 0
-        } else if (operatorArray[i].value == "-"){
-            operator = 1
-        } else if (operatorArray[i].value == "*") {
-            operator = 2
-        }else if (operatorArray[i].value == "/") {
-            operator = 3
+        operator = this.value;
+        if (opState == 0) {
+            firstNumber = display.value
+            firstFloat = parseFloat(firstNumber)
+            opState = 1;
+            display.value += operator;
+            console.log(operator)
+            lastOp = this.value
+        } else if(opState == 1) {
+            subDisplay.textContent = display.value
+            //Splits the expression at the operator so values can be extracted
+            let splitVal = display.value.split(lastOp)
+            firstFloat = parseFloat(splitVal[0]);
+            secondFloat = parseFloat(splitVal[1])
+                display.value = (`${operate(firstFloat, secondFloat, lastOp)}${operator}`)
+                lastOp = this.value;
+        }})};
+
+//Clear Button
+clearBtn = document.querySelector("#clear");
+clearBtn.addEventListener("click", function() {
+    display.value = ""
+    subDisplay.textContent = ""
+    firstNumber = ""
+    secondNumber = ""
+    firstFloat = undefined
+    secondFloat = undefined
+    opState = 0
+});
+
+//Enter "Equals" Button
+equalsBtn = document.querySelector("#equals");
+equalsBtn.addEventListener("click", function() {
+    if (opState == 1) {
+        let splitVal = display.value.split(operator)
+            firstFloat = parseFloat(splitVal[0]);
+            secondFloat = parseFloat(splitVal[1])
+            console.log(splitVal)
+    console.log(firstFloat)
+    console.log(secondFloat)
+    }
+    display.value = operate(firstFloat,secondFloat,operator)
+    subDisplay.textContent = ""
+    opState = 0
+});
+
+//Number Keys - These will update the display with the number you pressed
+numKeyList = document.querySelectorAll(".numKey");
+numKeyArray = Array.from(numKeyList);
+for (let i = 0; i < numKeyArray.length; i++) {
+    numKeyArray[i].addEventListener("click", function() {
+        display.value += numKeyArray[i].value
+        if (opState == 1 || opState == 2) {
+            let splitVal = display.value.split(/[+-/*]+/)
+            firstNumber = splitVal[0]
+            secondNumber = splitVal[1]
         }
-    });
+    })
 };
 
-//Add Function
-add = (firstValue, secondValue) => (firstValue + secondValue);
-
-//Subtract Function
-subtract = (firstValue, secondValue) => (firstValue - secondValue);
-
-//Multiply Function
-multiply = (firstValue, secondValue) => (firstValue * secondValue);
-
-//Divide Function
-divide = (firstValue, secondValue) => (firstValue / secondValue);
-
 //Operate Function
-let operateBtn = document.querySelector("#operate")
-operateBtn.addEventListener("click", function() {
-    if (operator == 0) {
-        console.log(add(firstInt, secondInt)
-    )} else if (operator == 1) {
-        console.log(subtract(firstInt, secondInt))
-    } else if (operator == 2) {
-        console.log(multiply(firstInt, secondInt))
-    } else if (operator == 3) {
-        console.log(divide(firstInt, secondInt))
+function operate(firstValue, secondValue, operatorVal) {
+    if (operatorVal == "+") {
+        return (firstValue + secondValue)
+    } else if (operatorVal == "-") {
+        return(firstValue - secondValue)
+    } else if (operatorVal == "*") {
+        return (firstValue * secondValue)
+    } else if (operatorVal == "/") {
+        return (firstValue /secondValue)
     }
-});
+};
